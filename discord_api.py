d297e4a4dcf0f3a336721e85f8481e3ccd713cbb
weesna <austingpeck@gmail.com>
2021-09-24 19:44:15 -0700

    initial commit

---
+++ discord_api.py
@@ -0,0 +1,5 @@
+#Goals:
+# User able specify their bot account
+# Bot able to observe chat rooms in the server for specific strings
+# Bot able to interact with the youtube portion of this script
+# Bot able to play music through input device
---
+++ youtube.py
@@ -0,0 +1,1 @@
+# This script will be designed to handle the youtube logic of searching/playing/maybe downloading
