5fdeb3807e9c8de20cc0e6411a143eab1d8b3c9b
weesna <austingpeck@gmail.com>
2021-09-24 20:20:48 -0700

    Probably more of an initial commit tbh

--- discord_api.py
+++ discord_api.py
@@ -1,4 +1,4 @@
-#Goals:
+# Goals:
 # User able specify their bot account
 # Bot able to observe chat rooms in the server for specific strings
 # Bot able to interact with the youtube portion of this script
---
+++ main.py
@@ -0,0 +1,4 @@
+# Main logic loop, accesses discord api and youtube to watch server and play music
+# logic for input/output devices contained in here or branch off??????
+
+
